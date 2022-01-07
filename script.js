687747729ce6dd26ab28c34560f99903da222979
Azeem <livelyazeem@gmail.com>
2022-01-07 16:10:09 +0500

    1

--- script.js
+++ script.js
@@ -17,4 +17,23 @@ a = 7;
 */
 
 // Assigning the Value of One Variable to Another
+/*
+var a;
+a = 7;
+var b;
+b = a;
+*/
+
+// Initializing Variables with the Assignment Operator
+/*
+var a = 9;
+*/
+
+// Declare String Variables
+/*
+var myFirstName = "Azeem";
+var myLastName = "Qamar";
+*/
+
+// Understanding Uninitialized Variables
 
