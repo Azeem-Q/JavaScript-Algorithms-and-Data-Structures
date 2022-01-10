33de44d5e9603537a68fc5b0f419d9e780b00f4f
Azeem <livelyazeem@gmail.com>
2022-01-10 12:48:00 +0500

    1

--- script.js
+++ script.js
@@ -47,4 +47,66 @@ c = c + " String!";
 */
 
 // Understanding Case Sensitivity in Variables
+/*
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+*/
+
+// Explore Differences Between the var and let Keywords
+/*
+let catName = "Oliver";
+let catSound = "Meow!";
+*/
+
+// Declare a Read-Only Variable with the const Keyword
+/*
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+
+console.log(FCC, fact);
+*/
+
+// Add Two Numbers with JavaScript
+/*
+const sum = 10 + 10;
+*/
+
+// Subtract One Number from Another with JavaScript
+/*
+const difference = 45 - 33;
+*/
+
+// Multiply Two Numbers with JavaScript
+/*
+const product = 8 * 10;
+*/
+
+// Divide One Number by Another with JavaScript
+/*
+const quotient = 66 / 33;
+*/
+
+// Increment a Number with JavaScript
+/*
+let myVar = 87;
+
+myVar = ++ myVar;
+
+console.log(myVar)
+*/
+
+// Decrement a Number with JavaScript
+/*
+let myVar = 11;
+myVar = -- myVar;
+
+console.log(myVar)
+*/
+
+// Create Decimal Numbers with JavaScript
