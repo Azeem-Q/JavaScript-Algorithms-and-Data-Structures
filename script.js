a277549d468c1efd244059f7680a455c1484db11
Azeem <livelyazeem@gmail.com>
2022-01-11 12:50:06 +0500

    1

--- script.js
+++ script.js
@@ -174,5 +174,35 @@ c /= 11;
 */
 
 // Escaping Literal Quotes in Strings
+/*
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+*/
 
+// Quoting Strings with Single Quotes
+/*
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+*/
+
+// Escape Sequences in Strings
+
+/*
+String Special Chars
+
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+
+*/
+/*
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+console.log(myStr);
+*/
+
+// Concatenating Strings with Plus Operator
 
