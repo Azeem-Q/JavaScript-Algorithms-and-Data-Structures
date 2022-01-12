27ece63d0b4e97645302babf0a317efeb2dbdc25
Azeem <livelyazeem@gmail.com>
2022-01-12 19:39:52 +0500

    learned till Local Scope and Functions

--- script.js
+++ script.js
@@ -368,4 +368,37 @@ functionWithArgs(3, 9);
 */
 
 // Return a Value from a Function with Return
+/*
+function timesFive(val) {
+    return val * 5;
+}
+const answer = timesFive(8);
+*/
 
+// Global Scope and Functions
+/*
+// Declare the myGlobal variable below this line
+
+let myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal here
+    oopsGlobal = 5;
+}
+
+// Only change the code above this line
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+fun2();
+*/
+
+// Local Scope and Functions
