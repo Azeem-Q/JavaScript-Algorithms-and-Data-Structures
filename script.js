48f6ee91df9addd91b4a13f648532b8563c9aa51
Azeem <livelyazeem@gmail.com>
2022-01-14 12:42:59 +0500

    learned till Comparison with the Strict Inequality Operator

--- script.js
+++ script.js
@@ -475,4 +475,62 @@ function welcomeToBooleans() {
 */
 
 // Use Conditional Logic with If Statements
+/*
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return 'Yes, that was true'
+    }
+    return 'No, that was false';
+}
+*/
+
+// Comparison with the Equality Operator
+/*
+function testEqual(val) {
+    if (val == 12) {
+        return 'Equal';
+    }
+    return 'Not Equal';
+}
+
+testEqual(10);
+*/
+
+// Comparison with the Strict Equality Operator
+/*
+function testStrict(val) {
+    if (val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testStrict(10);
+*/
+
+// Practice comparing different values
+/*
+function compareEquality(a, b) {
+    if (a === b) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+compareEquality(10, "10");
+*/
+
+// Comparison with the Inequality Operator
+/*
+function testNotEqual(val) {
+    if (val != 99) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+testNotEqual(10);
+*/
+
+// Comparison with the Strict Inequality Operator
 
