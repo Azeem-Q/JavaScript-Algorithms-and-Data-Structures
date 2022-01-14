e2f9416d13ed4c711d98d8a5d8a2d1edcb423060
Azeem <livelyazeem@gmail.com>
2022-01-14 17:24:11 +0500

    learned till Comparison with the Greater Than Or Equal To Operator

--- script.js
+++ script.js
@@ -533,4 +533,30 @@ testNotEqual(10);
 */
 
 // Comparison with the Strict Inequality Operator
+/*
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+testStrictNotEqual(10);
+*/
+
+// Comparison with the Greater Than Operator
+/*
+function testGreaterThan(val) {
+    if (val > 100) {
+        return "Over 100";
+    }
+    if (val > 10) {
+        return "Over 10";
+    }
+    return "10 or Under";
+}
+
+testGreaterThan(10);
+*/
+
+// Comparison with the Greater Than Or Equal To Operator
 
