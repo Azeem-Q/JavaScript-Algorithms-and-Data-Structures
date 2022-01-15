7f21613b4fed5c3989f26bb74fb6a3a2d748fd29
Azeem <livelyazeem@gmail.com>
2022-01-15 11:01:19 +0500

    learned till Comparisons with the Logical And Operator

--- script.js
+++ script.js
@@ -573,3 +573,32 @@ testGreaterOrEqual(10);
 */
 
 // Comparison with the Less Than Operator
+/*
+function testLessThan(val) {
+    if (val < 25) {
+        return "Under 25";
+    }
+    if (val < 55) {
+        return "Under 55";
+    }
+    return "55 or Over";
+}
+testLessThan(10);
+*/
+
+// Comparison with the Less Than Or Equal To Operator
+/*
+function testLessOrEqual(val) {
+    if (val <= 12) {
+        return "Smaller Than or Equal to 12";
+    }
+    if (val <= 24) {
+        return "Smaller Than or Equal to 24";
+    }
+    return "More Than 24";
+}
+testLessOrEqual(10);
+*/
+
+// Comparisons with the Logical And Operator
+
