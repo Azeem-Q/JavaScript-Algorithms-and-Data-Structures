20263c31cb96cdb86bbc55dcfad3c53a8c91b694
Azeem <livelyazeem@gmail.com>
2022-01-20 15:56:25 +0500

    learning Record Collection

--- script.js
+++ script.js
@@ -1023,4 +1023,57 @@ const gloveBoxContents = myStorage.car.inside["glove box"];
 */
 
 // Accessing Nested Arrays
+/*
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
+*/
 
+// Record Collection
+
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: [
+            'Let It Rock',
+            'You Give Love a Bad Name'
+        ]
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: [
+            '1999', 
+            'Little Red Corvette'
+        ]
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+function updateRecords(records, id, artist, trackks, val) {
+    return records
+}
