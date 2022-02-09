294fd05f4cec44008b29b2371bc5dab9a5903732
Azeem <livelyazeem@gmail.com>
2022-02-09 09:45:54 +0500

    learned till Use the Rest Parameter with Function Parameters

--- script.js
+++ script.js
@@ -1452,3 +1452,34 @@ console.log(magic);
 
 // Write Arrow Functions with Parameters
 
+/*
+Just like a regular function, you can pass arguments into an arrow function.
+
+const doubler = (item) => item * 2;
+doubler(4);
+doubler(4) would return the value 8.
+
+If an arrow function has a single parameter, the parentheses enclosing the parameter may be omitted.
+
+const doubler = item => item * 2;
+It is possible to pass more than one argument into an arrow function.
+
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2);
+multiplier(4, 2) would return the value 8.
+*/
+/*
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+*/
+
+// Set Default Parameters for Your Functions
+/*
+const increment = (number, value = 1) => number + value;
+
+console.log(increment(2));
+*/
+
+// Use the Rest Parameter with Function Parameters
+
