b84bd279667b4acade38fa572e9456ff70f36b35
Azeem <livelyazeem@gmail.com>
2022-02-09 08:59:19 +0500

    learned till Write Arrow Functions with Parameters

--- script.js
+++ script.js
@@ -1383,4 +1383,72 @@ editInPlace();
 */
 
 // Prevent Object Mutation
+/*
+let obj = {
+    name:"FreeCodeCamp",
+    review:"Awesome"
+  };
+  Object.freeze(obj);
+  obj.review = "bad";
+  obj.newProp = "Test";
+  console.log(obj); 
+*/
+/*
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    Object.freeze(MATH_CONSTANTS);
+    try {
+        MATH_CONSTANTS.PI = 99;
+    }   catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj()
+
+console.log(PI);
+*/
+
+// Use Arrow Functions to Write Concise Anonymous Functions
+
+/* In JavaScript, we often don't need to name our functions, 
+especially when passing a function as an argument to another 
+function. Instead, we create inline functions. We don't need 
+to name these functions because we do not reuse them anywhere 
+else.
+
+To achieve this, we often use the following syntax:
+
+const myFunc = function() {
+  const myVar = "value";
+  return myVar;
+}
+ES6 provides us with the syntactic sugar to not have to 
+write anonymous functions this way. Instead, you can use 
+arrow function syntax:
+
+const myFunc = () => {
+  const myVar = "value";
+  return myVar;
+}
+When there is no function body, and only a return value, 
+arrow function syntax allows you to omit the keyword return 
+as well as the brackets surrounding the code. This helps 
+simplify smaller functions into one-line statements:
+
+// const myFunc = () => "value";
+// This code will still return the string value by default.
+
+console.log(myFunc);
+*/
+/*
+const magic = () => new Date();
+
+console.log(magic);
+*/
+
+// Write Arrow Functions with Parameters
 
