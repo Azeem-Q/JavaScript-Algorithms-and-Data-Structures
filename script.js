5cd00912134f917fed3979a9fde1af4005aff21f
Azeem <livelyazeem@gmail.com>
2022-02-10 09:41:49 +0500

    learned till Write Concise Object Literal Declarations Using Object Property Shorthand

--- script.js
+++ script.js
@@ -1602,4 +1602,52 @@ console.log(a, b);
 */
 
 // Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+/*
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    const [a, b, ...arr] = list;
+    return arr;
+}
+
+const arr = removeFirstTwo(source);
+console.log(arr);
+*/
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+/*
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};"
+
+const half = ({max, min}) => (max + min) / 2;
+
+console.log(half(stats));
+*/
+
+// Create Strings using Template Literals
+/*
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList);
+*/
+
+// Write Concise Object Literal Declarations Using Object Property Shorthand
 
