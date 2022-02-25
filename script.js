5d902b96470960e49e94a06aa4d8134aa0b4625e
Azeem <livelyazeem@gmail.com>
2022-02-25 08:41:34 +0500

    learned till Catch Unclosed Parentheses, Brackets, Braces and Quotes

--- script.js
+++ script.js
@@ -2130,4 +2130,38 @@ console.log(result);
 // JS Debugging
 
 // Use the JavaScript Console to Check the Value of a Variable
+/*
+let a = 5;
+let b = 1;
+a++;
+console.log(a);
+let sumAB = a + b;
+console.log(sumAB);
+*/
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+/*
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
+*/
+
+// Use typeof to Check the Type of a Variable
+/*
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof(seven));
+console.log(typeof(three));5
+*/
+
+// Catch Misspelled Variable and Function Names
+/*
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+*/
+
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
 
