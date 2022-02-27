c5e046db931881a511edcc76d2ffaa5c700d3510
Azeem <livelyazeem@gmail.com>
2022-02-27 09:47:39 +0500

    learned till Add Items Using splice()

--- script.js
+++ script.js
@@ -2290,4 +2290,22 @@ console.log(mixedNumbers(['IV', 5, 'six']));
 */
 
 // Remove Items from an Array with pop() and shift()
+/*
+function popShift(arr) {
+    let popped = arr.pop();
+    let shifted = arr.shift();
+    return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+*/
+
+// Remove Items Using splice()
+/*
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1, 4);
+console.log(arr);
+*/
+
+// Add Items Using splice()
 
