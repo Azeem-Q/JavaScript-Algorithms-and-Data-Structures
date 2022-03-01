062dec256f3f2a941804755526879dcef1b98a08
Azeem <livelyazeem@gmail.com>
2022-03-01 12:27:04 +0500

    learned till Create complex multi-dimensional arrays

--- script.js
+++ script.js
@@ -2362,10 +2362,21 @@ console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 */
 
 // Iterate Through All an Array's Items Using For Loops
-
+/*
 function filteredArray(arr, elem) {
     let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(elem) < 0) {
+            //console.log(i.indexOf(elem))
+            newArr.push(arr[i]);
+        }   else {
+            continue;
+        }
+    }
     return newArr;
 }
 
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
+*/
+
+// Create complex multi-dimensional arrays
