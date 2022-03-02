bfc2737730d9507c919cab6243359a18a21d0534
Azeem <livelyazeem@gmail.com>
2022-03-02 12:27:35 +0500

    learned till Iterate Through the Keys of an Object with a for...in Statement

--- script.js
+++ script.js
@@ -2455,4 +2455,36 @@ console.log(foods);
 */
 
 // Check if an Object has a Property
+/*
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj){
+    if ('Alan' in userObj && 'Jeff' in userObj && 'Sarah' in userObj && 'Ryan' in userObj) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+console.log(isEveryoneHere(users));
+*/
+
+// Iterate Through the Keys of an Object with a for...in Statement
 
