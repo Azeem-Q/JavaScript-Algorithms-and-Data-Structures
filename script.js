a65227be33fc3a5e9abc1ea32f36feafca1af102
Azeem <livelyazeem@gmail.com>
2022-03-03 22:08:54 +0500

    learned till Understand Own Properties

--- script.js
+++ script.js
@@ -2862,4 +2862,59 @@ console.log(dog.sayLegs());
 */
 
 // Make Code More Reusable with the this Keyword
+/*
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return `This dog has ${this.numLegs} legs.`}
+};
+
+console.log(dog.sayLegs());
+*/
+
+// Define a Constructor Function
+/*
+function Dog() {
+    this.name = "Rocky";
+    this.color = "Brown";
+    this.numLegs = 4
+}
+*/
+
+// Use a Constructor to Create Objects
+/*
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+
+let hound = new Dog();
+*/
+
+// Extend Constructors to Receive Arguments
+/*
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+}
+
+let terrier = new Dog("Rocky", "White");
+
+console.log(terrier);
+*/
+
+// Verify an Object's Constructor with instanceof
+/*
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(2);
+
+console.log(myHouse instanceof House);
+*/
+
+// Understand Own Properties
 
