a0a5c38a6d368c19bb48f26d1a5878b17bb39088
Azeem <livelyazeem@gmail.com>
2022-03-09 18:31:33 +0500

    learning Understand the Hazards of Using Imperative Code

--- script.js
+++ script.js
@@ -3293,3 +3293,35 @@ console.log(
 
 // Understand the Hazards of Using Imperative Code
 
+const Window = function(tabs) {
+    this.tab = tabs;
+};
+
+Window.prototype.join = function(otherWindow) {
+    this.tabs = this.tabs.concat(otherWindow.tabs);
+    return this;
+};
+
+Window.prototype.tabOpen = function(tab) {
+    this.tabs.push('new tab');
+    return this;
+};
+
+Window.prototype.tabClose = function(index) {
+    const tabsBeforeIndex = this.tabs.splice(0, index);
+    const tabsAfterIndex = this.tabs.splice(index + 1);
+    this.tabs = tabsBeforeIndex.concat(tabsAfterIndex);
+    return this;
+};
+
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
+
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']);
+
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']);
+
+const finalTabs = socialWindow.tabOpen()
+.join(videoWindow.tabClose(2))
+.join(workWindow.tabClose(1).tabOpen());
+
+console.log(finalTabs.tabs);
