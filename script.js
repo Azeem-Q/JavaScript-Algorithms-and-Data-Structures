d64d7536dba7e207672a9eed6f98874e75605801
Azeem <livelyazeem@gmail.com>
2022-03-09 15:57:41 +0500

    learned till Understand Functional Programming Terminology

--- script.js
+++ script.js
@@ -3250,4 +3250,23 @@ bird.glide();
 // JS Functional Programming
 
 // Learn About Functional Programming
+/*
+const prepareTea = () => 'greenTea';
+
+const getTea = (numOfCups) => {
+    const teaCups = [];
+
+    for (let cups = 1; cups <= numOfCups; cups += 1) {
+        const teaCup = prepareTea();
+        teaCups.push(teaCup);
+    }
+    return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
+
+console.log(tea4TeamFCC);
+*/
+
+// Understand Functional Programming Terminology
 
