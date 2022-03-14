1a50d615e42967866a7636a5d7a1f208fe7d9641
Azeem <livelyazeem@gmail.com>
2022-03-14 12:44:46 +0500

    learning Use the map Method to Extract Data from an Array

--- script.js
+++ script.js
@@ -3377,3 +3377,12 @@ console.log(bookList);
 
 // Use the map Method to Extract Data from an Array
 
+const users = [
+    { name: 'John', age: 34 },
+    { name: 'Amy', age: 20 },
+    { name: 'camperCat', age: 10 }
+];
+  
+const names = users.map(user => user.name);
+console.log(names);
+
