88e5880f39b5ef8078c24733a295dc30b77bcbca
Azeem <livelyazeem@gmail.com>
2022-03-29 11:26:27 +0500

    learned till // Make a Person

--- script.js
+++ script.js
@@ -4405,4 +4405,25 @@ console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: 
 */
 
 // Arguments Optional
+/*
+function addTogether() {
+    if (arguments.length == 1 && typeof arguments[0] == 'number') {
+        return num => {
+            if (typeof num == 'number') {
+                return num + arguments[0];
+            }   else {
+                return undefined;
+            }
+        }
+    }   else if (arguments.length == 2 && typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
+        return arguments[0] + arguments[1];
+    }   else {
+        return undefined;
+    }
+}
+
+console.log(addTogether(2)([3]));
+*/
+
+// Make a Person
 
